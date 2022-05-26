c89cc32eafc463b31ddf59e62c1a717a70cdc063
Madeline King <madelk2@uci.edu>
2022-05-26 14:27:28 -0400

    Board Update and Play Token

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -191,7 +191,7 @@ socket.on('join_room_response', (payload) => {
    nodeA.show("fade",1000);
 
     /* Announcing in the chat that someone has arrived */
-    let newHTML = '<p class=\'join_room_response\'>'+payload.username+' joined the '+payload.room+'. (There are '+payload.count+' users in this room)</p>';
+    let newHTML = '<p class=\'join_room_response\'>'+payload.username+' joined the chatroom. (There are '+payload.count+' users in this room)</p>';
     let newNode = $(newHTML);
     newNode.hide();
     $('#messages').prepend(newNode);
@@ -244,6 +244,127 @@ socket.on('send_chat_message_response', (payload) =>{
     newNode.hide();
     $('#messages').prepend(newNode);
     newNode.show("fade", 2000);
+})
+
+
+
+let old_board = [
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?'],
+    ['?', '?', '?', '?', '?', '?', '?', '?']
+];
+
+let my_color = "";
+
+socket.on('game_update', (payload) =>{
+    if ((typeof payload == 'undefined') || (payload === null)){
+        console.log('Server did not send a payload');
+        return;
+    }
+    if(payload.result === 'fail'){
+        console.log(payload.message);
+        return;
+    }
+
+    let board = payload.game.board;
+    if ((typeof board == 'undefined') || (board === null)){
+        console.log('Server did not send a valid board to display');
+        return;
+    }
+
+    /* Update my color */
+    if (socket.id === payload.game.player_blue.socket) {
+        my_color = 'blue';
+    }
+     else if (socket.id === payload.game.player_black.socket) {
+        my_color = 'black';
+    }
+    else {
+       window.location.href= 'lobby.html?username=' + username;
+       return;
+    }
+
+    $("#my_color").html('<h3 id="my_color">I am '+my_color+'</h3>');
+
+
+
+    /* Animate changes to the board */
+    for (let row = 0; row < 8; row++) {
+       for (let column = 0; column < 8; column++) {
+           /* Check to see if the server changed any spaces on the board */
+           if(old_board[row][column] !== board[row][column]) {
+               let graphic = "";
+               let altTag = "";
+               if((old_board[row][column] === '?') && (board[row][column] === ' ')) {
+                   graphic = "empty.gif";
+                   altTag = "empty space";
+               }
+               else if((old_board[row][column] === '?') && (board[row][column] === 'u')) {
+                graphic = "empty to blue.gif";
+                altTag = "blue token";
+               }
+               else if((old_board[row][column] === '?') && (board[row][column] === 'b')) {
+                graphic = "empty to black.gif";
+                altTag = "black token";
+               }
+               else if((old_board[row][column] === ' ') && (board[row][column] === 'u')) {
+                graphic = "empty to blue.gif";
+                altTag = "blue token";
+               }
+               else if((old_board[row][column] === ' ') && (board[row][column] === 'b')) {
+                graphic = "empty to black.gif";
+                altTag = "black token";
+               }
+               else if((old_board[row][column] === 'u') && (board[row][column] === ' ')) {
+                graphic = "blue to empty.gif";
+                altTag = "empty space";
+               }
+               else if((old_board[row][column] === 'b') && (board[row][column] === ' ')) {
+                graphic = "black to empty.gif";
+                altTag = "empty space";
+               }
+               else if((old_board[row][column] === 'u') && (board[row][column] === 'b')) {
+                graphic = "blue to black.gif";
+                altTag = "black token";
+               }
+               else if((old_board[row][column] === 'b') && (board[row][column] === 'u')) {
+                graphic = "black to blue.gif";
+                altTag = "blue token";
+               }
+               else {
+                graphic = "error.gif";
+                altTag = "error";
+               }
+
+               const t = Date.now();
+               $('#'+row+'_'+column).html('<img class="imag-fluid" src="assets/images/'+graphic+'?time='+t+'" alt="'+altTag+'" />');     
+               $('#'+row+'_'+column).off('click');
+               if (board[row][column] === ' ') {
+                    $('#'+row+'_'+column).addClass('hovered_over'); 
+                    $('#'+row+'_'+column).click(((r,c) => {
+                        return(() => {
+                            let payload = {
+                                row: r,
+                                column: c,
+                                color: my_color
+                            };
+                            console.log('**** Client log message, sending \'play_token\' command: ' +JSON.stringify(payload));
+                            socket.emit('play_token', payload);
+                        });
+                    })(row,column));    
+                }  
+                else {
+                    $('#'+row+'_'+column).removeClass('hovered_over'); 
+                }          
+           }
+       }
+    }
+old_board = board;
 })
 
 /* Request to join the chat room */
